c52676c3ea1ac5c6d9b292f533396af05931c1fe
Apurva <apurva8904@gmail.com>
2021-12-17 02:00:48 +0530

    autocomplete react input example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [inputValue, setValue] = useState("");
+  const [suggestion, setSuggestion] = useState([]);
+
+  let options = [
+    "Atom",
+    "Atoni",
+    "Bill",
+    "Charlie",
+    "David",
+    "Emily",
+    "Ford",
+    "Garry",
+    "Henry",
+    "Ivy",
+    "Jill",
+    "Kevin",
+    "Kyle",
+    "Liz",
+    "Manny",
+    "Nick",
+    "Patrick",
+    "Owen",
+    "Quayle",
+    "Rob",
+    "Sam",
+    "Tom",
+    "Urvi",
+    "Venesa",
+    "Walker",
+    "Xman",
+    "Yoo",
+    "Zebra",
+    "Fred",
+  ];
+  const onChangeHandle = (e) => {
+    setValue(e.target.value);
+    setSuggestion([]);
+    e.target.value &&
+      options.forEach((s) => {
+        if (
+          s.substring(0, e.target.value.length).toUpperCase() ===
+          e.target.value.toUpperCase()
+        ) {
+          setSuggestion((suggestion) => suggestion.concat(s));
+          //console.log(suggestion)
+        }
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Autocomplete Name Input</h2>
+      <input
+        className="autocomplete"
+        onChange={(e) => onChangeHandle(e)}
+        value={inputValue}
+        placeholder="Type Name"
+      />
+      <div className="suggestions">
+        {suggestion.map((s, id) => {
+          return (
+            <li
+              key={id}
+              onClick={(e) => {
+                setValue(s);
+                setSuggestion([]);
+              }}
+            >
+              {s}
+            </li>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
